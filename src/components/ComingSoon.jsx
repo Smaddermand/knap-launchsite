5166864cef695b6d13964fafc2271851c548da84
Smaddermand <50303469+Smaddermand@users.noreply.github.com>
2024-05-20 08:13:48 +0200

    setup image and draft text

--- src/components/ComingSoon.jsx
+++ src/components/ComingSoon.jsx
@@ -6,12 +6,28 @@ export default function ComingSoon() {
         <div className="coming-soon">
             <div className="coming-soon-image-section">
                 <p>
-                    Insert Image here
+                    <img src="src/assets/knap-removebg-shadow.png" alt="" />
                 </p>
             </div>
             <div className="coming-soon-signup-section">
                 <p>
-                    Coming soon section
+                # Coming Soon: Knap
+
+                ## Keep Your Habits on Track
+
+                Say hello to **Knap**! It’s a fun, low-tech button that helps you remember your daily tasks. When Knap shows red, it’s a gentle reminder to get things done. Press it, and it turns green, signaling you're all set.
+
+                ### How It Works
+
+                - **Red**: Time to do your task.
+                - **Green**: Task done!
+
+                No apps, no fuss, just a friendly nudge. Knap makes it easy to stay on top of your goals.
+
+                ### Stay in the Loop
+
+                Enter your email below to receive updates and launch notifications. Don’t miss out!
+
                 </p>
             </div>
 
