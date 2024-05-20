f0d0dce6cc32d654cab6aa5df92ba7045db30cf8
Smaddermand <50303469+Smaddermand@users.noreply.github.com>
2024-05-20 07:38:49 +0200

    create initial layout setup and styling

---
+++ src/components/ComingSoon.jsx
@@ -0,0 +1,21 @@
+// ComingSoon.jsx
+
+export default function ComingSoon() {
+
+    return (
+        <div className="coming-soon">
+            <div className="coming-soon-image-section">
+                <p>
+                    Insert Image here
+                </p>
+            </div>
+            <div className="coming-soon-signup-section">
+                <p>
+                    Coming soon section
+                </p>
+            </div>
+
+        </div>
+    )
+
+}
---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+// header.jsx
+
+export default function Header() {
+
+    return (
+        <div className="header">
+
+            <h2>knap</h2>
+
+        </div>
+    )
+
+}
